664f842eb66f27c4782d880ddcc6dc56691e3c8e
priya3667 <webbybutter.priya@gmail.com>
2023-06-23 12:24:35 +0530

    first commit

---
+++ angular-workspace/projects/component-library/src/lib/component-library.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ComponentLibraryComponent } from './component-library.component';
+
+describe('ComponentLibraryComponent', () => {
+  let component: ComponentLibraryComponent;
+  let fixture: ComponentFixture<ComponentLibraryComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [ComponentLibraryComponent]
+    });
+    fixture = TestBed.createComponent(ComponentLibraryComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
